cf99f99d0247831080448b2bd9d1790a331e1d01
jdefeza01 <jdefeza01@educarex.es>
2025-02-05 10:40:21 +0100

    Modificacion realizada por Javier de Fez Arregui

--- src/vectores/UsoArrays.java
+++ src/vectores/UsoArrays.java
@@ -6,6 +6,10 @@ public class UsoArrays {
 	public static void main(String[] args) {
 		// TODO Esbozo de metodo generado automaticamente
 
+		
+	//Este procedimiento se ha modificado por Javier de Fez Arregui
+		
+		
 		int opcion=Integer.parseInt(JOptionPane.showInputDialog("Dame una opcion (1-2)"));
 
 		if(opcion==1) {
